8144d02acdf3dc06d963011ce79c86f0392354b2
Robert Kaye <rob@partyrobotics.com>
2011-08-19 23:58:55 -0700

    The zero crossings idle detection algorithm was suffering from DC offset problems, so I investigated looking at deriv zero crossings. I don't think this is going to work well -- I'm hoping the DC removal stuff that Ray is working on will allow my preferred idle detection to work again.

--- ballz/fsm.c
+++ ballz/fsm.c
@@ -26,13 +26,14 @@ typedef struct
     uint8_t new_state;
 } Transition;
 
-#define NUM_TRANSITIONS 7
+#define NUM_TRANSITIONS 8
 Transition transition_table[NUM_TRANSITIONS] = 
 {
     { STATE_ZERO_POINT,            TRANSITION_PERIOD_FINDER,  STATE_PERIOD_FINDER         },
     { STATE_PERIOD_FINDER,         TRANSITION_SWINGING,       STATE_IDLE              },
 //    { STATE_PERIOD_FINDER,         TRANSITION_SWINGING,       STATE_SWINGING              },
     { STATE_SWINGING,              TRANSITION_IDLE,           STATE_IDLE                  },
+    { STATE_SWINGING,              TRANSITION_PULL_UP,        STATE_PULL_UP               },
     { STATE_IDLE,                  TRANSITION_ZERO_POINT,     STATE_ZERO_POINT            },
     { STATE_IDLE,                  TRANSITION_PULL_UP,        STATE_PULL_UP               },
     { STATE_PULL_UP,               TRANSITION_IDLE    ,       STATE_IDLE                  },
@@ -42,7 +43,6 @@ Transition transition_table[NUM_TRANSITIONS] =
 #define PULL_UP_THRESHOLD_Y .2
 #define PULL_UP_THRESHOLD_T .5
 
-#define NUM_PULL_UP_HISTORY_POINTS    16
 #define SWINGING_IDLE_ZERO_CROSSING_THRESHOLD_T .5
 
 int sign(float val)
@@ -143,7 +143,7 @@ void process_data_period_finder(uint8_t state, __unused vector *a, __unused vect
             period_reg = 2 * period_ball; // 2 = 1 << (filter order = 1) to adjust for final scaling
         period_ball = lowpass(&period_reg, 1, period_ball);
         period_data_valid = 1;
-        dprintf("period: %f\n", period_ball);
+        //dprintf("period: %f\n", period_ball);
         //printf("%f %f %f %f\n", t, a->z, da->z, period_ball);
     }
     else
@@ -157,20 +157,30 @@ void process_data_period_finder(uint8_t state, __unused vector *a, __unused vect
 }
 
 static float   t_last_zero_cross = 0.0;
+static float   t_last_deriv_zero_cross = 0.0;
 static uint8_t moveToPullUp = 0;
 static uint8_t moveToIdle = 0;
 static uint8_t moveToSwinging = 0;
 void process_data_idle(uint8_t state, vector *a, vector *da, float t)
 {
+    static float last_dy = 0.0;
+
     if ((a->y > 0.0 && a->y - da->y <= 0.0) || (a->y < 0.0 && a->y - da->y >= 0.0))
         t_last_zero_cross = t;
+    if ((da->y > 0.0 && last_dy <= 0.0) || (da->y < 0.0 && last_dy >= 0.0))
+        t_last_deriv_zero_cross = t;
 
     if (t - t_last_zero_cross > PULL_UP_THRESHOLD_T && fabs(a->y) > PULL_UP_THRESHOLD_Y)
         moveToPullUp = 1;
 
+    last_dy = da->y;
+
     //dprintf("%f %f %f\n", t, a->y, da->y);
 }
 
+// If the absolute value of the derivative of the Z axis
+// goes above this threshold, we will consider the user to have let go of the ball.
+#define SWING_START_DERIV_THRESHOLD_Z .01
 void process_data_pull_up(uint8_t state, vector *a, vector *da, float t)
 {
     // If we're seeing zero crossings, then the ball is near idle again
@@ -191,7 +201,7 @@ void process_data_pull_up(uint8_t state, vector *a, vector *da, float t)
             cbi(PORTC, 2);
             sbi(PORTC, 3);
         }
-        if (fabs(da->y) > .02)
+        if (fabs(da->z) > SWING_START_DERIV_THRESHOLD_Z)
         {
             moveToSwinging = 1;
             //dprintf("da: %f\n", da->y);
@@ -200,7 +210,7 @@ void process_data_pull_up(uint8_t state, vector *a, vector *da, float t)
 }
 
 #define IDLE_ACCEL_Y .05
-#define IDLE_DERIV_Y .01
+#define IDLE_DERIV_Y .0015
 #define IDLE_THRESHOLD_T 1.00 
 float last_non_idle_t = 0.0;
 void process_data_swinging(uint8_t state, vector *a, vector *da, float t)
@@ -223,7 +233,7 @@ uint8_t state_idle(uint8_t prev_state, float t)
 {
     if (prev_state != STATE_IDLE)
     {
-        //dprintf("State: idle\n");
+        dprintf("State: idle\n");
         // Turn on only the blue LED
         sbi(PORTC, 1);
         sbi(PORTC, 2);
@@ -242,7 +252,7 @@ uint8_t state_pull_up(uint8_t prev_state, float t)
 {
     if (prev_state != STATE_PULL_UP)
     {
-        //dprintf("State: pull up\n");
+        dprintf("State: pull up\n");
         // Turn on only the red LED to start
         cbi(PORTC, 1);
         sbi(PORTC, 2);
@@ -269,7 +279,7 @@ uint8_t state_swinging(uint8_t prev_state, float t)
 {
     if (prev_state != STATE_SWINGING)
     {
-        //dprintf("State: swinging\n");
+        dprintf("State: swinging\n");
         cbi(PORTC, 1);
         sbi(PORTC, 2);
         cbi(PORTC, 3);
@@ -277,6 +287,14 @@ uint8_t state_swinging(uint8_t prev_state, float t)
 
     if (t - last_non_idle_t > IDLE_THRESHOLD_T)
         return TRANSITION_IDLE;
+    //if (t - t_last_deriv_zero_cross < PULL_UP_THRESHOLD_T)
+    //    return TRANSITION_IDLE;
+
+    if (moveToPullUp)
+    {
+        moveToPullUp = 0;
+        return TRANSITION_PULL_UP;
+    }
 
     return TRANSITION_NO_CHANGE;
 }
@@ -307,11 +325,12 @@ void fsm_loop(void)
 //            dprintf("Samples: %d\n", samples);
 //        }
 
-        dprintf("%f %f %f\n", t, a.z, da.z);
+//        dprintf("%f %f %f\n", t, a.z, da.z);
 
         // Replace a and da with lowpass filtered versions of the same
         lowpassv(&a_lp_reg, 5, &a);
         lowpassv(&da_lp_reg, 5, &da);
+//        dprintf("%f %f %f\n", t, a.y, da.y);
 
         process_data_peaks(STATE_SWINGING, &a, &da, t);
         process_data_zero_point(state, &a, &da, t);
