f52dd79ab2ca0dc227f0a482ca9f12523cb47091
Robert Kaye <rob@partyrobotics.com>
2012-04-17 19:56:27 -0700

    Adding missing files

---
+++ ballz/hue.h
@@ -0,0 +1,13 @@
+#ifndef __HUE_H__
+#define __HUE_H__
+
+#define HUE_MAX           252 
+
+typedef struct
+{
+    uint8_t red, green, blue;
+} color_t;
+
+void color_hue(uint8_t h, color_t *c) ;
+
+#endif
